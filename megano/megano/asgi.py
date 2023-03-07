9d26f0e57e40168c1459f82d1ac702113f058e08
Alexander Tarasov <work.acc.em@gmail.com>
2023-03-07 14:16:46 +0300

    создан проект megano, удален старый проект и подключен пакет frontend

--- megano/megano/asgi.py
+++ megano/megano/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for buy_here project.
+ASGI config for megano project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buy_here.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'megano.settings')
 
 application = get_asgi_application()
--- megano/megano/urls.py
+++ megano/megano/urls.py
@@ -1,4 +1,4 @@
-"""buy_here URL Configuration
+"""megano URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
